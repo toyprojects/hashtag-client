e1342ae14e09fb4d58f35a93b84e47e5d35b1daf
getsolaris <getsolaris@naver.com>
2018-12-02 01:36:06 +0900

    add: left banner frame complete

--- src/components/LeftBanner.js
+++ src/components/LeftBanner.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+
+// font awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faHeart, faUser, faShoppingBasket } from '@fortawesome/free-solid-svg-icons'
+library.add(faBars, faHeart, faUser, faShoppingBasket)
 
 const Menu = styled.div`
   background-color: white;
@@ -11,13 +17,76 @@ const Menu = styled.div`
   display: inline-block;
 `
 
+const LogoArea = styled.div`
+  padding-bottom: 60px;
+`
+
+const MenuArea = styled.div`
+  font-weight: 100;
+`
+
+const LineSeparator = styled.div`
+  width: 180px;
+  height: 1px;
+  background-color: #222;
+  margin: 20px 0 30px -12px;
+`
+
+const Wrap = styled.div`
+  margin: 64px 48px 0;
+`
+
+const StyledLink = styled(Link)`
+  background-color: transparent !important;
+  background-image: none !important;
+  border-color: transparent;
+  border: none;
+  cursor: pointer;
+  font-weight: 100;
+  text-decoration: none;
+  color: black;
+
+  &:hover {
+    color: black;
+    text-decoration: none;
+  }
+`
+
+const LineWrap = styled.div`
+  text-align: left;
+  height: 30px;
+`
+
 class LeftBanner extends Component {
   render() {
     return (
       <Menu className="banner">
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="signin">로그인</NavLink></li>
-        <li><NavLink to="signup">회원가입</NavLink></li>
+        <Wrap>
+          <LogoArea>
+            <StyledLink to="/" style={{ textDecoration: 'none', color: 'black' }}>
+              <h3>Logo Image</h3>
+            </StyledLink>
+          </LogoArea>
+          <MenuArea>
+            <StyledLink to="/signin">로그인</StyledLink> 
+            <span style={{ padding: '0px 10px 0px 10px' }}>|</span>
+            <StyledLink to="/signup">회원가입</StyledLink>
+          </MenuArea>
+          <LineSeparator />
+          <LineWrap>
+            <StyledLink to="/"><FontAwesomeIcon icon="bars" /> 카테고리</StyledLink>
+          </LineWrap>
+          <LineWrap>
+            <StyledLink to="/"><FontAwesomeIcon icon="heart" /> 하트</StyledLink>
+          </LineWrap>
+          <LineWrap>
+            <StyledLink to="/"><FontAwesomeIcon icon="user" /> 마이페이지</StyledLink>
+          </LineWrap>
+          <LineSeparator />
+          <LineWrap>
+            <StyledLink to="/"><FontAwesomeIcon icon="shopping-basket" /> 장바구니</StyledLink>
+          </LineWrap>
+        </Wrap>
       </Menu>
     );
   }
