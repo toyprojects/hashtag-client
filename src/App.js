383a1e84e8b963cffc450030c5a754370e6d2768
myungjaeyu <rootcode10@gmail.com>
2018-12-07 10:26:38 +0900

    수정 : URL의 path가 / 또는 today가 포함 시 HomeMenu가 렌더링되도록 리펙토링

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route, HashRouter, BrowserRouter, Switch } from 'react-router-dom'
+import { Route, withRouter, Switch } from 'react-router-dom'
 import styled from 'styled-components'
 import LeftBanner from './components/LeftBanner'
 import RightBanner from './components/RightBanner'
@@ -35,9 +35,9 @@ class App extends Component {
 
 
   render() {
-    function renderMenu() {
-      const URL = ['#/', '#/today/ranking', '#/today/new', '#/today/reward', '#/today/benefit', '#/today/basket', '#/today/event']
-      return URL.indexOf(window.location.hash) >= 0 && <HomeMenu />;
+  
+    function renderMenu(pathname) {
+      return (pathname === '/' || pathname.indexOf('today') !== -1) && <HomeMenu />
     }
   
     return (
@@ -51,7 +51,7 @@ class App extends Component {
               color={'#2979ff'}
               loading={this.state.loading}
             /> */}
-            { renderMenu() }
+            { renderMenu(this.props.location.pathname) }
             <Switch>
               { routes.map(route => <Route key={route.path} {... route} />) }}
             </Switch>
@@ -62,4 +62,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(App);
