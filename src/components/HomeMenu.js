ed129f5d0abb5539b281a7d233c06875dbff9dd2
getsolaris <getsolaris@naver.com>
2018-12-12 01:17:37 +0900

    add search box frame

--- src/components/HomeMenu.js
+++ src/components/HomeMenu.js
@@ -5,7 +5,7 @@ import Swiper from 'react-id-swiper'
 
 const SwipeMenu = styled.div`
   border-bottom: 1px solid #eee;
-  margin-top: 70px;
+  margin-top: 15px;
   padding-bottom: 10px;
   margin-bottom: 15px;
   height: 40px;
@@ -43,9 +43,26 @@ const StyledLink = styled(NavLink)`
 const SwiperParentApp = {
   width: 'auto',
   display: 'inline-block',
-  marginLeft: '40px',
-  marginRight: '40px'
+  marginLeft: '33px'
 }
+
+const SearchContainer = styled.div`
+  padding-top: 20px;
+  text-align: left;
+`
+
+const SearchBar = styled.div`
+  width: 87%;
+  border: 1px solid #ccc;
+  height: 40px;
+  border-radius: 20px;
+  padding: 0 20px 0 20px;
+  margin: 0 auto;
+  line-height: 35px;
+  font-size: 13px;
+  color: #222;
+  font-weight: 100;
+`
 
 class HomeMenu extends Component {
   render() {
@@ -57,31 +74,38 @@ class HomeMenu extends Component {
       },
     };
     return (
-      <SwipeMenu>
-        <Swiper {...params}>
-          <StyledLink to="/" exact="true" style={ SwiperParentApp }>
-            <div className="inner-slide-content">홈</div>
-          </StyledLink>
-          <StyledLink to="/today/ranking" style={ SwiperParentApp }>
-            <div className="inner-slide-content">오늘랭킹</div>
-          </StyledLink>
-          <StyledLink to="/today/new" style={ SwiperParentApp }>
-            <div className="inner-slide-content">오늘신상</div>
-          </StyledLink>
-          <StyledLink to="/today/reward" style={ SwiperParentApp }>
-            <div className="inner-slide-content">오늘리워드</div>
-          </StyledLink>
-          <StyledLink to="/today/benefit" style={ SwiperParentApp }>
-            <div className="inner-slide-content">오늘혜택</div>
-          </StyledLink>
-          <StyledLink to="/today/basket" style={ SwiperParentApp }>
-            <div className="inner-slide-content">오늘찜순위</div>
-          </StyledLink>
-          <StyledLink to="/today/event" style={ SwiperParentApp }>
-            <div className="inner-slide-content">오늘이벤트</div>
-          </StyledLink>
-        </Swiper>
-      </SwipeMenu>
+      <div>
+        <SearchContainer>
+          <SearchBar>
+            <span>원하는 검색어로 최저가 상품들을 !</span>
+          </SearchBar>
+        </SearchContainer>
+        <SwipeMenu>
+          <Swiper {...params}>
+            <StyledLink to="/" exact="true" style={ SwiperParentApp }>
+              <div className="inner-slide-content">홈</div>
+            </StyledLink>
+            <StyledLink to="/today/ranking" style={ SwiperParentApp }>
+              <div className="inner-slide-content">오늘랭킹</div>
+            </StyledLink>
+            <StyledLink to="/today/new" style={ SwiperParentApp }>
+              <div className="inner-slide-content">오늘신상</div>
+            </StyledLink>
+            <StyledLink to="/today/reward" style={ SwiperParentApp }>
+              <div className="inner-slide-content">오늘리워드</div>
+            </StyledLink>
+            <StyledLink to="/today/benefit" style={ SwiperParentApp }>
+              <div className="inner-slide-content">오늘혜택</div>
+            </StyledLink>
+            <StyledLink to="/today/basket" style={ SwiperParentApp }>
+              <div className="inner-slide-content">오늘찜순위</div>
+            </StyledLink>
+            <StyledLink to="/today/event" style={ SwiperParentApp }>
+              <div className="inner-slide-content">오늘이벤트</div>
+            </StyledLink>
+          </Swiper>
+        </SwipeMenu>
+      </div>
     )
   }
 }
